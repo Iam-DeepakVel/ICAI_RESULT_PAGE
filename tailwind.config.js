be8f654362ec47206aba1937ccc56ccc49998833
Iam-DeepakVel <deepak.vel82@gmail.com>
2022-10-30 16:22:08 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-50": "#0798F9",
+        "primary-100": "#0275C2",
+      },
+    },
+  },
+  plugins: [],
+};
